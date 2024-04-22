874931d62c69c2c72c679eca3d84edcc47d8ae6d
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2024-04-22 23:31:18 +0700

    Enermy

--- Assets/EnermyScissors.cs
+++ Assets/EnermyScissors.cs
@@ -1,62 +1,39 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnermyScissors : MonoBehaviour
 {
-    public float speed;
-    private GameObject player;
-    public bool chase = false;
-    public Transform startPoint;
-    public bool CanWalk;
-
-    private void Start()
+    [SerializeField] float moveSpeed = 1f;
+    bool isFacingRigibody;
+    Rigidbody2D rb;
+    BoxCollider2D myBoxCollider;
+    void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-
+        rb = GetComponent<Rigidbody2D>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
     }
 
-    private void Update()
+    void Update()
     {
-        if (player == null)
-            return;
-        if (chase == true)
-            Chase();
-        else
+        if (IsFacingRigibody())
         {
-            ReturnStartPoint();
-            Flip();
-        }
-    }
-
-    private void Flip()
-    {
-        if (transform.position.x > player.transform.position.x)
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        else
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-    }
-
-    private void ReturnStartPoint()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime); 
-    }
-
-    private void Chase()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ground"))
-        {
-            _ = CanWalk == true;
+            rb.velocity = new Vector2(moveSpeed, 0f);
         }
         else
         {
-            _ = CanWalk == false;
+            rb.velocity = new Vector2(-moveSpeed, 0f);
         }
+    }
+
+    private bool IsFacingRigibody()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+
+    }
+      
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
     }
 }
