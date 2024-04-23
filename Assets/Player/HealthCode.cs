f9bf491983fb8bb5c57d53e85d87bd50e48bcb5d
LoganChan <akanenitiraks@gmail.com>
2024-04-23 14:42:47 +0700

    Update2

---
+++ Assets/Player/HealthCode.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthCode : MonoBehaviour
+{
+   public float Health = 5;
+
+   void Start ()
+   {
+      if (Health <= 0)
+      {  
+
+      }
+   }
+}
